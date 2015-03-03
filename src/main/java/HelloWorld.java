1d9c52b047ec0086e8529681a53ea08794fd4609
Neil Ellis <neil@cazcade.com>
2015-03-03 16:29:19 +0000

    Moved over to a java example to better illustrate the flexibility of Codeserver

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,8 +1,22 @@
-import static spark.Spark.*;
+import com.mongodb.*;
+
+import java.util.Date;
+
+import static spark.Spark.get;
+import static spark.Spark.port;
 
 public class HelloWorld {
     public static void main(String[] args) {
         port(8080);
-        get("/", (req, res) -> "Hello World");
+        get("/", (req, res) -> {
+            MongoClient mongoClient = new MongoClient(new MongoClientURI(System.getenv("MONGODB_URL")));
+            DB db = mongoClient.getDB("example");
+            DBCollection table = db.getCollection("visitors");
+            BasicDBObject document = new BasicDBObject();
+            document.put("ip", req.ip());
+            document.put("createdDate", new Date());
+            table.insert(document);
+            return "Hello " + req.ip() + "You are the " + table.count() + " visitor";
+        });
     }
 }
